60fe10a2a8b68f8b41eda242b85b3426581e2e6e
delwiv <delwiv@protonmail.com>
2023-10-21 12:17:13 +0200

    feat: fork my own shopify seed

--- packages/store/app/components/Hero.tsx
+++
@@ -1,9 +0,0 @@
-const Hero = ({img, title, text}) => {
-  return (
-    <div className="hero">
-      <img src={'https://www.example.com/image.jpg'} alt="" />
-      <p>Hello, world!</p>
-    </div>
-  );
-};
-export default Hero;
